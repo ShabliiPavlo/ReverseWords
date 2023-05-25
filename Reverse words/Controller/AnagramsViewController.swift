62ff72d408f7b492ef163580f05397b6fb7b5a57
ShabliiPavloEdit profileCustomize your pinsShabliiPavloEdit profileCustomize your pinsShabliiPavloEdit profileCustomize your pins <>
2023-05-25 23:04:32 +0300

    fix: extension AnagramsViewController, fix: class AnagramsModel

--- Reverse words/Controller/AnagramsViewController.swift
+++ Reverse words/Controller/AnagramsViewController.swift
@@ -11,9 +11,11 @@ class AnagramsViewController: UIViewController {
     
     @IBOutlet weak var switcherTextType: UISegmentedControl!
     @IBOutlet weak var anagramsTextToReverse: UITextField!
-    @IBOutlet weak var resulttextLable: UILabel!
+    @IBOutlet weak var resultTextLable: UILabel!
     @IBOutlet weak var resultDescription: UILabel!
     @IBOutlet weak var anagramsTextToIgnor: UITextField!
+    
+    @IBOutlet weak var switcherMods: UISegmentedControl!
     
     let anagramsModel = AnagramsModel()
     
@@ -21,33 +23,54 @@ class AnagramsViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    @IBAction func switcherReversType(_ sender: UISegmentedControl) {
+    @IBAction func switchReverseType(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
             anagramsTextToIgnor.isHidden = true
             resultDescription.isHidden = false
+            updateResultLabel()
         } else {
             anagramsTextToIgnor.isHidden = false
             resultDescription.isHidden = true
+            updateResultLabel()
         }
     }
-    
 }
 
 extension AnagramsViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-
-            guard let currentText = textField.text,
-                  let updatedText = (currentText as NSString?)?.replacingCharacters(in: range, with: string) else {
-                return true
-            }
-
-            let reversedText = anagramsModel.reverseTextOnly(updatedText)
-            resulttextLable.text = reversedText
-
-            textField.text = updatedText
-
-            return false
+        
+        guard let currentTextReverse = anagramsTextToReverse.text,
+              let updatedTextReverse = (currentTextReverse as NSString?)?.replacingCharacters(in: range, with: string) else {
+            return true
         }
+        
+        let reversedText: String
+        if switcherMods.selectedSegmentIndex == 0 {
+            reversedText = anagramsModel.reverseTextOnly(updatedTextReverse)
+        } else {
+            reversedText = anagramsModel.reverseText(updatedTextReverse)
+        }
+        
+        resultTextLable.text = reversedText
+        anagramsTextToReverse.text = updatedTextReverse
+        
+        return false
+    }
+    
+    func updateResultLabel() {
+        guard let currentTextReverse = anagramsTextToReverse.text else {
+            return
+        }
+        
+        let reversedText: String
+        if switcherMods.selectedSegmentIndex == 0 {
+            reversedText = anagramsModel.reverseTextOnly(currentTextReverse)
+        } else {
+            reversedText = anagramsModel.reverseText(currentTextReverse)
+        }
+        
+        resultTextLable.text = reversedText
+    }
 }
 
 
--- Reverse words/Model/AnagramsModel.swift
+++ Reverse words/Model/AnagramsModel.swift
@@ -31,4 +31,13 @@ class AnagramsModel {
         }
         return reversedText
     }
+    
+    func reverseText(_ text: String) -> String {
+        let separatedWords = text.components(separatedBy: " ")
+        let reversedWords = separatedWords.map { String($0.reversed()) }
+        let reversedText = reversedWords.joined(separator: " ")
+        return reversedText
+    }
 }
+
+
